69eb894616798cd0883153378634c527fe643ea8
Julien Dupont <julien.dupont@student-cs.fr>
2023-10-20 10:09:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='back'>
+			<div className='bg'></div>
+			<div className='container'>
+				<div id='text'>Text</div>
+				<div id='option-buttons' className='btn-grid'>
+					<button className='btn'>Option 1</button>
+					<button className='btn'>Option 2</button>
+					<button className='btn'>Option 3</button>
+					<button className='btn'>Option 4</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
